ae009f91d5b97ae56a2b7a36d124281c89b7da14
Maksim Zolotoi <crmapache@icloud.com>
2023-04-10 15:31:44 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const onClick = async () => {
+    const convertValue = (value) => {
+      return value.replaceAll('.','')
+        .replaceAll('#','')
+        .replaceAll("-", '')
+        .replaceAll("/", '')
+        .split(/\s+/)
+        .map((item, index) => {
+          if (index === 0) {
+            return item.toLowerCase()
+          } else {
+            return item.charAt(0).toUpperCase() + item.slice(1)}}).join('')
+    }
+    
+    const value = await navigator.clipboard.readText()
+    await navigator.clipboard.writeText(convertValue(value));
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClick}>CONVERT ME</button>
     </div>
   );
 }
